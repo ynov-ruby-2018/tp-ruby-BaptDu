8aa7b3ae4c17b5df8c839cdfd607ed477ff6ec41
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 17:00:05 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'posts' => 'posts#index'
+  get 'posts' => 'posts#index' # Doublon avec root.. et du coup, HomeController ne sert à rien ?
   post 'posts' => 'posts#create'
 
-  root to: 'posts#index'
+  root to: 'posts#index' 
 end
